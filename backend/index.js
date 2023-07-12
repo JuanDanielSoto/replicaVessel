ac7c0c1a05bbf206cd7a59042e22dd29f629222f
Juan Daniel Soto Montes <34689368+JuanDanielSoto@users.noreply.github.com>
2023-07-12 13:28:21 -0600

    Update index.js
    Allow CORS for google cloud

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,11 @@ dbConnection();
 app.use( express.static('public') );
 
 // CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use( cors() );
 
 // Lectura y parseo del body
